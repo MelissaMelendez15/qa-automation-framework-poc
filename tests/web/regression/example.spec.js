3831e9e1aad5ae54043ad26b1a28f1ce8e065743
MelissaMelendez15 <MelissaMelendez15@gmail.com>
2026-04-19 13:29:52 +0200

    fix: Se restaura el estado de aprobación después de una prueba de fallo intencional

--- tests/web/regression/example.spec.js
+++ tests/web/regression/example.spec.js
@@ -2,5 +2,5 @@ const { test, expect } = require('@playwright/test');
 
 test('regression - validar contenido visible', async ({ page }) => {
     await page.goto('https://www.example.com/');
-    await expect(page.locator('h1')).toHaveText('Texto Incorrecto');
+    await expect(page.locator('h1')).toHaveText('Example Domain');
 });
